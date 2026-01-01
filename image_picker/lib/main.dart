d9d3c364f3c5bf47a4df1fadd593e4fc09158a8d
Nisitha529 <silvanisithamaneesha@gmail.com>
2026-01-01 15:45:01 +0530

    img_sel_func_implt
    Image selection functions and process implementation.

--- image_picker/lib/main.dart
+++ image_picker/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -54,17 +57,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final ImagePicker _picker = ImagePicker ();
+  File? _image;
+
+  chooseImages () async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState ((){
+        _image = File(image.path);
+      });
+    }
+  }
+
+  captureImages () async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    if (image != null) {
+      setState ((){
+        _image = File(image.path);
+      });
+    }
   }
 
   @override
@@ -104,19 +116,16 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            _image != null ? Image.file(_image!) : Icon ( Icons.image, size : 150, ),
+            ElevatedButton (onPressed : () {
+              chooseImages();
+            }, onLongPress : () {
+              captureImages();
+            }, child:  Text ("Choose / Capture"))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
