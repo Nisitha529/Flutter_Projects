c91a9e0e02cab302d8fcf2e3ac4e97e621e18656
Nisitha529 <silvanisithamaneesha@gmail.com>
2026-01-21 23:57:04 +0530

    real_time_image_classification

--- img_prc/lib/main.dart
+++ img_prc/lib/main.dart
@@ -1,83 +1,177 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 
 late List<CameraDescription> _cameras;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   _cameras = await availableCameras();
-  runApp(const MyApp());
+  runApp(const CameraScreen());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CameraScreen> createState() => _CameraScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _CameraScreenState extends State<CameraScreen> {
   late CameraController controller;
+  CameraImage? img;
+  bool isBusy = false;
+  String result = "";
 
+  dynamic imageLabeler;
   @override
   void initState() {
     super.initState();
-    controller = CameraController(_cameras[1], ResolutionPreset.max);
+
+    final ImageLabelerOptions options = ImageLabelerOptions(
+      confidenceThreshold: 0.5,
+    );
+    imageLabeler = ImageLabeler(options: options);
+
+    controller = CameraController(
+      _cameras[1],
+      ResolutionPreset.high,
+      imageFormatGroup: Platform.isAndroid
+          ? ImageFormatGroup.nv21
+          : ImageFormatGroup.bgra8888,
+    );
     controller
         .initialize()
         .then((_) {
-      if (!mounted) {
-        return;
-      }
+          if (!mounted) {
+            return;
+          }
 
-      controller.startImageStream((image) {
-        print (image.width.toString() + "   " + image.height.toString());
-      });
+          controller.startImageStream(
+            (image) => {
+              if (!isBusy) {isBusy = true, img = image, doImageLabeling(image)},
+            },
+          );
 
-      setState(() {});
-    })
+          setState(() {});
+        })
         .catchError((Object e) {
-      if (e is CameraException) {
-        switch (e.code) {
-          case 'CameraAccessDenied':
-          // Handle access errors here.
-            break;
-          default:
-          // Handle other errors here.
-            break;
-        }
-      }
+          if (e is CameraException) {
+            switch (e.code) {
+              case 'CameraAccessDenied':
+                print('User denied camera access.');
+                break;
+              default:
+                print('Handle other errors.');
+                break;
+            }
+          }
+        });
+  }
+
+  doImageLabeling(CameraImage img) async {
+    result = "";
+    InputImage? inputImg = getInputImage(img);
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImg!);
+
+    for (ImageLabel label in labels) {
+      final String text = label.label;
+      final int index = label.index;
+      final double confidence = label.confidence;
+      result += text + "  " + confidence.toStringAsFixed(2) + "\n";
+    }
+
+    setState(() {
+      result;
+      isBusy = false;
     });
+  }
+
+  final _orientations = {
+    DeviceOrientation.portraitUp: 0,
+    DeviceOrientation.landscapeLeft: 90,
+    DeviceOrientation.portraitDown: 180,
+    DeviceOrientation.landscapeRight: 270,
+  };
+
+  InputImage? getInputImage(CameraImage image) {
+    final camera = _cameras[1];
+    final sensorOrientation = camera.sensorOrientation;
+
+    InputImageRotation? rotation;
+
+    if (Platform.isIOS) {
+      rotation = InputImageRotationValue.fromRawValue(sensorOrientation);
+    } else if (Platform.isAndroid) {
+      var rotationCompensation =
+          _orientations[controller!.value.deviceOrientation];
+
+      if (rotationCompensation == null) return null;
+      if (camera.lensDirection == CameraLensDirection.front) {
+        rotationCompensation = (sensorOrientation + rotationCompensation) % 360;
+      } else {
+        rotationCompensation =
+            (sensorOrientation - rotationCompensation + 360) % 360;
+      }
+
+      rotation = InputImageRotationValue.fromRawValue(rotationCompensation);
+    }
+
+    if (rotation == null) return null;
+
+    final format = InputImageFormatValue.fromRawValue(image.format.raw);
+
+    if (format == null ||
+        (Platform.isAndroid && format != InputImageFormat.nv21) ||
+        (Platform.isIOS && format != InputImageFormat.bgra8888))
+      return null;
+
+    if (img?.planes.length != 1) return null;
+
+    final plane = img?.planes.first;
+
+    return InputImage.fromBytes(
+      bytes: plane!.bytes,
+      metadata: InputImageMetadata(
+        size: Size(img!.width.toDouble(), image.height.toDouble()),
+        rotation: rotation,
+        format: format,
+        bytesPerRow: plane.bytesPerRow,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: controller.value.isInitialized ? CameraPreview(controller) : Container()
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: Stack(
+        fit: StackFit.expand,
+        children: [
+          CameraPreview(controller),
+          Container(
+            margin: const EdgeInsets.only(left: 10, bottom: 10),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                result,
+                style: const TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- img_prc/my_app/android/app/build.gradle.kts
+++ img_prc/my_app/android/app/build.gradle.kts
@@ -6,26 +6,26 @@ plugins {
 }
 
 android {
-    namespace = "com.example.img_prc"
-    compileSdk = 36 // flutter.compileSdkVersion
+    namespace = "com.example.my_app"
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.img_prc"
+        applicationId = "com.example.my_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion // flutter.minSdkVersion
-        targetSdk = 36 //flutter.targetSdkVersion
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
